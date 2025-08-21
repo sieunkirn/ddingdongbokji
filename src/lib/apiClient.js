e605ad28570d8163623df732d91621b0a5c08534
김시은 <sieunkirn@gmail.com>
2025-08-21 22:15:35 +0900

    Update apiClient.js

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,22 +1,10 @@
-// // src/lib/apiClient.js
-// console.log("API BASE =", import.meta.env.VITE_API_BASE_URL);
-//
-// import axios from 'axios';
-//
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_BASE_URL,
-//     timeout: 15000,
-// });
-//
-// export default api;
-
 // src/lib/apiClient.js
 import axios from 'axios'
 
-// 우선순위: 환경변수 > ngrok > localhost
+// 우선순위: 환경변수 > Render 배포 > localhost
 const API_BASE =
     import.meta.env.VITE_API_BASE_URL ||
-    'https://d32cc7c8eb4b.ngrok-free.app' ||
+    'https://silvercare-backend.onrender.com' ||
     'http://localhost:8080'
 
 const api = axios.create({
@@ -30,3 +18,26 @@ const api = axios.create({
 })
 
 export default api
+
+//
+//
+// // src/lib/apiClient.js
+// import axios from 'axios'
+//
+// // 우선순위: 환경변수 > ngrok > localhost
+// const API_BASE =
+//     import.meta.env.VITE_API_BASE_URL ||
+//     'https://d32cc7c8eb4b.ngrok-free.app' ||
+//     'http://localhost:8080'
+//
+// const api = axios.create({
+//     baseURL: API_BASE,
+//     timeout: 15000,
+//     withCredentials: false,
+//     headers: {
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json',
+//     },
+// })
+//
+// export default api
